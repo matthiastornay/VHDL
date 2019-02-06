e8494c537f9bd75aacf33bc20cb408e07d3f1d51
matthiastornay <47396178+matthiastornay@users.noreply.github.com>
2019-02-06 19:44:54 +0100

    Update and rename Demultiplexeur_4_parmi_1_V1.vhd to Demux_1_to_4_V1.vhd

--- Demux_1_to_4_V1.vhd
+++ Demux_1_to_4_V1.vhd
@@ -1,20 +1,20 @@
--- Démultiplexeur 4C1
+-- Demultiplexer 1 to 4
 -- Matthias Tornay
 
--- Déclaration des Librairies
+-- Libraries declaration
 library ieee;
 use ieee.std_logic_1164.all;
 
--- Description de l'entité
-entity Demultiplexeur_4C1 is
+-- Entity description
+entity demux_4C1 is
      port(E, D : in std_logic;
 	  Sel : in std_logic_vector (1 downto 0);
 	  S0, S1, S2, S3 : out std_logic);
 
-end Demultiplexeur_4C1;
+end demux_4C1;
 
--- Déclaration du comportement
-architecture arch_Demultiplexeur_4C1 of Demultiplexeur_4C1 is
+-- Behavior
+architecture arch_demux_4C1 of demux_4C1 is
 
 begin
      S0 <= D when (Sel = "00") and (E = '0') else '1';
@@ -22,4 +22,4 @@ begin
      S2 <= D when (Sel = "10") and (E = '0') else '1';
      S3 <= D when (Sel = "11") and (E = '0') else '1';
 	
-end arch_Demultiplexeur_4C1;
+end arch_demux_4C1;
