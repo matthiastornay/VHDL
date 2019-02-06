c151da30d417e57fdbcaae2ed5e0549bbedd6d7e
matthiastornay <47396178+matthiastornay@users.noreply.github.com>
2019-02-06 19:14:55 +0100

    Update OR_2_bits.vhd

--- OR_2_bits.vhd
+++ OR_2_bits.vhd
@@ -1,21 +1,21 @@
 -- OR 2 bits
 -- Matthias Tornay
 
--- Déclaration des Librairies
+-- DÃ©claration des Librairies
 library ieee;
 use ieee.std_logic_1164.all;
 
--- Description de l'entité
+-- Description de l'entitÃ©
 entity OR_2B is
      port(A, B : in std_logic;
 	  S : out std_logic);
 
 end OR_2B;
 
--- Déclaration du comportement
+-- DÃ©claration du comportement
 architecture arch_OR_2B of OR_2B is
 
 begin
      S <= A OR B;
 
-end arch_OR_2;
+end arch_OR_2B;
