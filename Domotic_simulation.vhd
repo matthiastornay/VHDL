22bffbf2727c53161ef715aaf2b86a7fea0d75fa
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-02-07 01:26:13 +0100

    Add files via upload

---
+++ Domotic_simulation.vhd
@@ -0,0 +1,22 @@
+-- Domotic simulation
+-- Matthias Tornay
+
+-- Libraries declaration
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Entity description
+entity simulation is
+	port(S, T, V : in std_logic;
+	     awning, fan, sprayer : out std_logic);
+
+end simulation;
+
+-- Behavior
+architecture arch_simulation of simulation is
+begin
+	awning  <= S and not V;
+ 	fan <= T and not V;
+ 	sprayer <= S and T and not V;
+
+end arch_VHDL;
