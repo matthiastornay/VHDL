330985c52e341f59d75969a937fe71ee2278f835
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-12-14 01:40:10 +0100

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -24,13 +24,13 @@ signal white_now : std_logic_vector(4 downto 0);
 begin
  
 	result <= ('0' & A) + ('0' & B) + ("0000" & Cin) when (OPF = "000") else
-		      ('0' & A) - ('0' & B) - ("0000" & Cin) when (OPF = "001") else
-		      "00000" when (OPF = "010") else
-		      ('0' & A) + ("0000" & Cin) when (OPF = "011") else
-		      '0' & not(A) when (OPF =  "100") else
-		      ('0' & A) or ('0' & B) when (OPF = "101") else
-		      ('0' & A) and ('0' & B) when (OPF =  "110") else
-		      ('0' & A) xor ('0' & B) when (OPF =  "111"); 
+	          ('0' & A) - ('0' & B) - ("0000" & Cin) when (OPF = "001") else
+	          "00000" when (OPF = "010") else
+	          ('0' & A) + ("0000" & Cin) when (OPF = "011") else
+	          '0' & not(A) when (OPF =  "100") else
+	          ('0' & A) or ('0' & B) when (OPF = "101") else
+	          ('0' & A) and ('0' & B) when (OPF =  "110") else
+	          ('0' & A) xor ('0' & B) when (OPF =  "111"); 
 		 
 	S <= result (3 downto 0);
 	Cout <= result (4) when OPF (2) = '0' else '0';
